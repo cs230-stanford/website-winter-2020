45bc65987bd97734106ad079537db1858087b495
Mohamed El-Geish <elgeish@gmail.com>
2019-09-29 21:34:11 -0700

    Set permissions and other misc. changes

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-JEKYLL_ENV=production jekyll build --baseurl '//cs230.stanford.edu' 
-rsync -rv ./_site/* rice:/afs/ir/class/cs230/WWW/
+set -e
+JEKYLL_ENV=production bundle exec jekyll build --baseurl '//cs230.stanford.edu' 
+rsync -rv --chmod=774 ./_site/* rice:/afs/ir/class/cs230/WWW/
