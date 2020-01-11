9c9b5749a5df062781555b5dd16e5398acd013de
Leo Mehr <leomehr@stanford.edu>
2020-01-11 12:39:41 -0800

    deploy.sh: update chmod args

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
 JEKYLL_ENV=production bundle exec jekyll build --baseurl '//cs230.stanford.edu' 
-rsync -rv --chmod=774 ./_site/* rice:/afs/ir/class/cs230/WWW/
+rsync -rv --chmod=Du=rwx,Dgo=rx,Fu=rw,Fgo=r ./_site/* rice:/afs/ir/class/cs230/WWW/
